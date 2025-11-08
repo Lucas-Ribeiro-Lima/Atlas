18a6a255594f85421ae7dee342e8f3a8f323dc58
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2025-11-08 11:00:17 -0300

    feat: Adjust the regex to use a single pattern.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from parser import parse_pdf
 from pdf2image import convert_from_path
 import pytesseract
 
-BASE_PATH = r"C:\Users\lucas.lima\Downloads\multas"
-OUTPUT_CSV = r"C:\Users\lucas.lima\Downloads\test.csv"
+
+BASE_PATH = r"C:\Users\lucas\Downloads\Multas\Cobrança de multas - 803 - 09-12-24.pdf"
+OUTPUT_CSV = r"C:\Users\lucas\Downloads\test.csv"
 
 is_new_file = not path.exists(OUTPUT_CSV)
 
@@ -16,15 +17,19 @@ def to_csv(kvp_dict):
         writer = csv.DictWriter(csvfile, fieldnames=kvp_dict.keys())
         if is_new_file:
             writer.writeheader()
+            is_new_file = False
         writer.writerow(kvp_dict)
+
+def process(images):
+    for image in images:
+        text_pdf = pytesseract.image_to_string(image)
+        to_csv(parse_pdf(text_pdf))
 
 def main():
     try:
-        for file in listdir(BASE_PATH):
-            images = convert_from_path(path.join(BASE_PATH, file))
-            for image in images:
-                text_pdf = pytesseract.image_to_string(image)
-                to_csv(parse_pdf(text_pdf))
+        images = convert_from_path(BASE_PATH)
+        process(images)
+
     except Exception as e:
         print(e)
     return
--- parser.py
+++ parser.py
@@ -1,74 +1,23 @@
 import re
 
-RE_EMISSAO = re.compile(
-    r"Data\s+de\s+Emiss[aã]o\s+(?P<DAT_EMISS>\d{2}/\d{2}/\d{4})",
-    re.IGNORECASE
-)
+RE_KVP = re.compile(
+    r"Data\s+de\s+Emiss[aãsd4]o\s+(?P<DAT_EMISS>\d{2}/\d{2}/\d{4}).*"
+    r"Data\s+do\s+Vencimento\s+(?P<DAT_VENC>\d{2}/\d{2}/\d{4}).*"
+    r"N[°º]?\s*Auto\s+de\s+Infra[cçgs][aãsd4]o:?\s+(?P<AI>[\w$-]{5,8}).*"
+    r"Concession[aád4]ria:?\s+(?P<CONC>[\w\s\-]+?)\s+Lan[cçgsd4]amento.*"
+    r"Linha:?\s+(?P<LINHA>[\w\s/\-]+?)\s+Ve[ií]culo:?\s+(?P<VEIC>\d{4,6})\s+"
+    r"Placa:?\s+(?P<PLACA>[A-Z0-9]{7})\s+"
+    r"Data:?\s+(?P<DAT_OCC>\d{2}/\d{2}/\d{4})\s+"
+    r"Hora:[^.]*?(?P<HORA_OCC>\d{2}:\d{2})\s+"
+    r"Local:?\s+(?P<LOCAL>[^.]*?)\sBase\slegal.*"
+    r"Descri[cçgs][aãsd4]o\s+da\s+infra[cçgs]ao:\s+(?P<DESC>[^.]+).*"
+    r"Valor:?\s+R\$\s*(?P<VALOR>[\d.,]+)"
+    , re.DOTALL | re.IGNORECASE | re.MULTILINE)
 
-RE_VENC = re.compile(
-    r"Data\s+do\s+Vencimento\s+(?P<DAT_VENC>\d{2}/\d{2}/\d{4})",
-    re.IGNORECASE
-)
-
-RE_AI = re.compile(
-    r"N[°º]?\s*Auto\s+de\s+Infra[cçgs][aã]o[:\s]*?(?P<AI>[\w$-]{5,8})",
-    re.IGNORECASE
-)
-
-RE_CONC = re.compile(
-    r"Concession[aá]ria[:\s]+(?P<CONC>[\w\s\-]+?)\s+Lan[cçgs]amento",
-    re.IGNORECASE
-)
-
-RE_LINHA = re.compile(
-    r"Linha[:\s]+(?P<LINHA>[\w\s/\-]+?)\s+Ve[ií]culo",
-    re.IGNORECASE
-)
-
-RE_VEIC = re.compile(
-    r"Ve[ií]culo[:\s]+(?P<VEIC>\d{4,6})",
-    re.IGNORECASE
-)
-
-RE_PLACA = re.compile(
-    r"Placa[:\s]+(?P<PLACA>[A-Z0-9]{7})",
-    re.IGNORECASE
-)
-
-RE_DATA_OCC = re.compile(
-    r"Data[:\s]+(?P<DAT_OCC>\d{2}/\d{2}/\d{4})",
-    re.IGNORECASE
-)
-
-RE_HORA_OCC = re.compile(
-    r"Hora[:\s]+(?P<HORA_OCC>\d{2}:\d{2})",
-    re.IGNORECASE
-)
-
-RE_LOCAL = re.compile(
-    r"Local[:\s]+(?P<LOCAL>[\w\s\-]{1,3})",
-    re.IGNORECASE
-)
-
-RE_DESC = re.compile(
-    r"Descri[cçgs][aã]o\s+da\s+infra[cçgs]ao[:\s]+(?P<DESC>[^.]+)",
-    re.IGNORECASE | re.DOTALL
-)
-
-# Valor
-RE_VALOR = re.compile(
-    r"Valor[:\s]+R\$\s*(?P<VALOR>[\d.,]+)",
-    re.IGNORECASE
-)
 
 def parse_pdf(pdf_data):
-    campos = {}
-    for regex in [
-        RE_EMISSAO, RE_VENC, RE_AI, RE_CONC, RE_LINHA, RE_VEIC,
-        RE_PLACA, RE_DATA_OCC, RE_HORA_OCC, RE_LOCAL, RE_DESC, RE_VALOR
-    ]:
-        match = regex.search(pdf_data)
-        if match:
-            campos.update(match.groupdict())
-    return campos
+    match = re.search(RE_KVP, pdf_data)
+    if not match:
+        raise Exception("Invalid PDF")
 
+    return match.groupdict()
